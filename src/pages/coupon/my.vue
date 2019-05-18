b13319514862493b7258127367e7230ed3c94700
ZoDream <648383079@qq.com>
2019-05-18 14:10:06 +0800

    修改

--- src/pages/coupon/my.vue
+++ src/pages/coupon/my.vue
@@ -40,18 +40,107 @@
 import {
     IMyApp
 } from '../../app';
-import { WxJson, WxPage } from '../../../typings/wx/lib.wx.page';
+import { WxJson, WxPage, TouchEvent } from '../../../typings/wx/lib.wx.page';
+import { getMyCoupon } from '../../api/coupon';
 const app = getApp<IMyApp>();
 
 interface IPageData {
+    statusList: string[],
+    status: number,
+    items: any[],
+    isExpand: boolean,
+    has_more: boolean,
+    page: number,
+    isLoading: boolean
 }
 @WxJson({
     navigationBarTitleText: "我的优惠券",
     navigationBarBackgroundColor: "#05a6b1",
     navigationBarTextStyle: "white",
+    enablePullDownRefresh: true,
+    onReachBottomDistance: 10,
 })
 export class My extends WxPage<IPageData> {
+    public data: IPageData = {
+        statusList: [
+            '未使用',
+            '使用记录',
+            '已过期',
+        ],
+        status: 0,
+        items: [],
+        isExpand: false,
+        has_more: true,
+        page: 1,
+        isLoading: false
+    }
 
+    public onLoad() {
+        this.tapRefresh();
+    }
+    onPullDownRefresh() {
+        this.tapRefresh();
+    }
+
+    onReachBottom() {
+        this.tapMore();
+    }
+
+    /**
+     * tapStatus
+     */
+    public tapStatus(e: TouchEvent) {
+        let status = e.currentTarget.dataset.status; 
+        if (this.data.status == status) {
+            return;
+        }
+        this.setData({
+            status: status
+        });
+        this.tapRefresh();
+    }
+
+    public tapRefresh() {
+        this.setData({
+            items: [],
+            isLoading: false,
+            has_more: true
+        });
+        this.goPage(1);
+    }
+
+    public tapMore() {
+        if (!this.data.has_more) {
+            return;
+        }  
+        this.goPage(this.data.page + 1);
+    }
+
+    public goPage(page: number) {
+        if (this.data.isLoading) {
+            return;
+        }
+        this.setData({
+            isLoading: true
+        });
+        getMyCoupon({
+            status: this.data.status,
+            page,
+        }).then(res => {
+            let items = [];
+            if (page < 2) {
+                items = res.data as never[];
+            } else {
+                items = [].concat(this.data.items as never[], res.data as never[]);
+            }
+            this.setData({
+                has_more: res.paging.more,
+                isLoading: false,
+                page,
+                items
+            });
+        });
+    }
 }
 </script>
 <style lang="scss" scoped>
