a3023ae2ece461abac04aa9746879adebef036e4
ZoDream <648383079@qq.com>
2019-09-13 19:48:04 +0800

    修改

--- src/pages/account/center.vue
+++ src/pages/account/center.vue
@@ -21,14 +21,7 @@ const app = getApp<IMyApp>();
 interface IPageData {
     items: IConnect[]
 }
-interface IConnectMapItem {
-    name?: string;
-    icon?: string;
-}
 
-interface IConnectMap {
-    [key: string]: IConnectMapItem
-}
 @WxJson({
     navigationBarTitleText: "账户安全",
     navigationBarBackgroundColor: "#05a6b1",
@@ -45,60 +38,10 @@ export class Center extends WxPage<IPageData> {
             if (!res.data) {
                 return;
             }
-            this.refresh(res.data);
+            this.setData({
+                items: res.data
+            });
         }); 
-    }
-
-    public refresh(items: IConnect[]) {
-        const maps: IConnectMap = {
-            qq: {
-                name: 'QQ',
-                icon: 'fa-qq'
-            },
-            wx: {
-                name: '微信',
-                icon: 'fa-weixin'
-            },
-            alipay: {
-                name: '支付宝',
-                icon: 'fa-alipay'
-            },
-            weibo: {
-                name: '微博',
-                icon: 'fa-weibo'
-            },
-            paypal: {
-                name: 'PayPal',
-                icon: 'fa-paypal'
-            },
-            github: {
-                name: 'Github',
-                icon: 'fa-github'
-            },
-            google: {
-                name: 'Google',
-                icon: 'fa-google'
-            },
-        };
-        for (const item of items) {
-            if (item.vendor && !maps.hasOwnProperty(item.vendor)) {
-                maps[item.vendor] = item;
-                continue;
-            }
-            if (!item.vendor) {
-                continue;
-            }
-            maps[item.vendor] = Object.assign(maps[item.vendor], item);
-        }
-        items = [];
-        for (const key in maps) {
-            if (maps.hasOwnProperty(key)) {
-                items.push(maps[key]);
-            }
-        }
-        this.setData({
-            items: items
-        });
     }
 }
 </script>
